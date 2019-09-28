ee00f5ee9afa7a48f29b3dbfcd75714bffd77428
Yasuyuki Abe <yasu@yasuabe.net>
2019-09-28 22:55:08 +0900

    add FromStdin.scala

--- src/main/scala/create_stream/FromStdin.scala
+++ src/main/scala/create_stream/FromStdin.scala
@@ -5,5 +5,6 @@ import fs2.{Stream, io, text}
 
 object FromStdin extends StreamDemoApp {
   def stream[F[_] : ConcurrentEffect : Timer : ContextShift](implicit bl: Blocker): Stream[F, String] =
-    io.stdin(4096, bl).through(text.utf8Decode)
+    io.stdin(4096, bl)
+      .through(text.utf8Decode)
 }
