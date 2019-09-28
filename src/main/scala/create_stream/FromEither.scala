c603e2ee503b3f74f2ebd58e7da654bcdf120959
Yasuyuki Abe <yasu@yasuabe.net>
2019-09-28 22:36:38 +0900

    add FromEither.scala

--- src/main/scala/create_stream/FromEither.scala
+++ src/main/scala/create_stream/FromEither.scala
@@ -1,6 +1,6 @@
 package create_stream
 
-import cats.effect.{Blocker, ConcurrentEffect, ContextShift, Sync, Timer}
+import cats.effect.{Blocker, ConcurrentEffect, ContextShift, ExitCode, IO, IOApp, Sync, Timer}
 import fs2.Stream
 import cats.syntax.either._
 
@@ -12,9 +12,16 @@ object FromEitherLeft extends StreamDemoApp {
   def stream[F[_] : ConcurrentEffect : Timer : ContextShift](implicit bl: Blocker): fs2.Stream[F, String] =
     Stream.fromEither[F](new Exception("test").asLeft[String])
 }
-object FromList extends StreamDemoApp {
-  val strings = LazyList("apple", "banana", "chocolate")
+object FromEitherIO extends IOApp {
+  import cats.syntax.functor._
+  import cats.syntax.applicativeError._
 
-  def stream[F[_] : ConcurrentEffect : Timer : ContextShift](implicit bl: Blocker): fs2.Stream[F, String] =
-    Stream.fromIterator(strings.iterator)
+  def program[F[_]](implicit F: Sync[F]): F[Unit] =
+    Stream.fromEither[F](new Exception("test").asLeft[String])
+      .compile.drain
+      .handleErrorWith(th => F.delay(println(th.getMessage)))
+
+  def run(args: List[String]): IO[ExitCode] =
+    program[IO] as ExitCode.Success
 }
+
